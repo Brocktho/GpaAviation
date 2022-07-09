76fc80cc7cde8712473dd882f960c762df69834b
Brocktho <bcd5235@psu.edu>
2022-07-09 18:53:12 -0400

    new changes

--- app/components/Navigation/Navbar.tsx
+++ app/components/Navigation/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 z-10 flex h-32 w-full flex-row justify-around border-b-2 border-slate-100 bg-slate-50 shadow-xl dark:bg-slate-700">
       <div className="w-1/5 bg-slate-400"></div>
-      <div className="w-2/5 bg-red-400"></div>
+      <div className="hidden flex-row bg-red-400 md:flex md:w-2/5 "></div>
       <div className="w-3/5 bg-green-400 md:w-1/5">
         {user ? (
           <Link
